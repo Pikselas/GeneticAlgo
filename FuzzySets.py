6c1fea615203b5505f2eee92d4ba7c56cf404215
kashikichi <60090083+Pikselas@users.noreply.github.com>
2023-12-13 11:46:05 +0530

    added fuzzy operations

---
+++ FuzzySets.py
@@ -0,0 +1,18 @@
+
+def FuzzyOperation(zipped_sets , Predicator):
+    Predication = []
+    for zipped_set in list(zipped_sets):
+        Predication.append(Predicator(zipped_set))
+    return Predication
+
+# calculate min & max of given fuzzy sets
+
+A = [0.2, 0.4, 0.6, 0.8]
+B = [0.1, 0.3, 0.5, 0.7]
+C = [0.2, 0.3, 0.4, 0.5]
+
+# Union
+print(FuzzyOperation(zip(A, B, C), min))
+
+# Intersection
+print(FuzzyOperation(zip(A, B, C), max))
